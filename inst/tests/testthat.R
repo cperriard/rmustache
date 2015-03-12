a1aed64154deea7676b828c23742e5cfa89f957f
Cédric Perriard <c@perriard.se>
2015-03-12 16:18:28 +0100

    updated test script

--- inst/tests/testthat.R
+++ inst/tests/testthat.R
@@ -9,8 +9,10 @@ inverted <- yaml.load_file("inst/tests/specs/inverted.yml")
 tests <- sections$tests
 
 context("Sections")
-for (test in tests) {
-    #test <- tests[[4]]
+for (i in c(1:25)
+     ) {
+    test <- tests[[i]]
+    print(i)
     test_that(test$name, 
               expect_that(render_mustache(test$template, test$data), 
                           equals(test$expected)
