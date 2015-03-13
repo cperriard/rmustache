0f6a7b980f460e1fa34cd8f7d61884af0187bdb6
Cédric <cedric.perriard@pensionsmyndigheten.se>
2015-03-13 10:19:30 +0100

    Context ported

--- R/Context.R
+++ R/Context.R
@@ -4,47 +4,50 @@ Context <- function Context(view, parentContext = NULL) {
     
     this <- new.env()
     this$view <- view
-    this$cache <- list('.' = this$view)
+    this$cache <- list('.' = this$view) ## why not an empty list?
     this$parent <- parentContext
     
-
+    
     # Creates a new context using the given view with this context as the parent.
     this$push <- function (view) {
-            return(Context(view, this))
+        return(Context(view, this))
+    }
+    
+    # Returns the value of the given name in this context, traversing
+    # up the context hierarchy if the value is absent in this context's view.
+    this$lookup <- function (name) {
+        if (name %in% names(this$cache)) {
+            value <- this$cache[[name]]
+        } else {
+            context <- this
+            while (!is.null(context)) {
+                if (str_detect(name, "\\.")) {
+                    value <- context$view;
+                    names <- unlist(str_split(name, "\\."))
+                    index <- 0
+                    while (!is.null(value) && index < length(names)) {
+                        value <- value[[names[index]]]
+                        index <- index + 1
+                    }
+                        
+                } else if (class(context$view) == "list") {
+                    value = context$view[[name]]
+                }
+                if (!is.null(value))
+                    break
+                context <- context$parent
+            }
+            this$cache[name] <- value
         }
-
+        if (class(value) == "function") {
+            value <- do.call(value, list(this = this$view)) 
+        }
+            
+        return(value)
+    }
+    
+    
     return(this)
 }
 
 
-/**
-    * Returns the value of the given name in this context, traversing
-* up the context hierarchy if the value is absent in this context's view.
-*/
-Context.prototype.lookup = function (name) {
-var cache = this.cache;
-var value;
-if (name in cache) {
-value = cache[name];
-} else {
-var context = this, names, index;
-while (context) {
-if (name.indexOf('.') > 0) {
-value = context.view;
-names = name.split('.');
-index = 0;
-while (value != null && index < names.length)
-value = value[names[index++]];
-} else if (typeof context.view == 'object') {
-value = context.view[name];
-}
-if (value != null)
-break;
-context = context.parent;
-}
-cache[name] = value;
-}
-if (isFunction(value))
-value = value.call(this.view);
-return value;
-};
